25126e5c5620e9e366d68c046c7344caad583f5a
Jose-Verdu-Diaz <pepv@hotmail.es>
2020-04-23 12:43:01 +0200

    Calculadora funcionando correctamente para cálculos de dos números

--- app/src/main/java/com/example/dsa_calculator/MainActivity.java
+++ app/src/main/java/com/example/dsa_calculator/MainActivity.java
@@ -11,26 +11,17 @@ public class MainActivity extends AppCompatActivity {
 
 /*------------------DEFINIR VARIABLES PARA CADA UNO DE LOS WIDGETS-----------------*/
 
-    private Button btn_one;
-    private Button btn_two;
-    private Button btn_three;
-    private Button btn_four;
-    private Button btn_five;
-    private Button btn_six;
-    private Button btn_seven;
-    private Button btn_eight;
-    private Button btn_nine;
-    private Button btn_zero;
-    private Button btn_add;
-    private Button btn_subtract;
-    private Button btn_multiply;
-    private Button btn_divide;
-    private Button btn_result;
-    private Button btn_clear;
+    private Button btn_one, btn_two, btn_three, btn_four, btn_five, btn_six, btn_seven, btn_eight,
+            btn_nine, btn_zero, btn_add, btn_subtract, btn_multiply, btn_divide, btn_result, btn_clear;
 
     private TextView txtview_resultview;
 
+    private boolean addition, subtraction, multiplication, division;
+
     /*----------------------------------------------------------------------------*/
+
+    private float  value1, value2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,98 +32,146 @@ public class MainActivity extends AppCompatActivity {
         btn_one.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                txtview_resultview.setText(txtview_resultview.getText().toString() + "1");
+                txtview_resultview.setText(txtview_resultview.getText() + "1");
             }
         });
 
         btn_two.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                txtview_resultview.setText(txtview_resultview.getText().toString() + "2");
+                txtview_resultview.setText(txtview_resultview.getText() + "2");
             }
         });
 
         btn_three.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                txtview_resultview.setText(txtview_resultview.getText().toString() + "3");
+                txtview_resultview.setText(txtview_resultview.getText() + "3");
             }
         });
 
         btn_four.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                txtview_resultview.setText(txtview_resultview.getText().toString() + "4");
+                txtview_resultview.setText(txtview_resultview.getText() + "4");
             }
         });
 
         btn_five.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                txtview_resultview.setText(txtview_resultview.getText().toString() + "5");
+                txtview_resultview.setText(txtview_resultview.getText() + "5");
             }
         });
 
         btn_six.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                txtview_resultview.setText(txtview_resultview.getText().toString() + "6");
+                txtview_resultview.setText(txtview_resultview.getText() + "6");
             }
         });
 
         btn_seven.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                txtview_resultview.setText(txtview_resultview.getText().toString() + "7");
+                txtview_resultview.setText(txtview_resultview.getText() + "7");
             }
         });
 
         btn_eight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                txtview_resultview.setText(txtview_resultview.getText().toString() + "8");
+                txtview_resultview.setText(txtview_resultview.getText()+ "8");
             }
         });
 
         btn_nine.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                txtview_resultview.setText(txtview_resultview.getText().toString() + "9");
+                txtview_resultview.setText(txtview_resultview.getText() + "9");
             }
         });
 
         btn_zero.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                txtview_resultview.setText(txtview_resultview.getText().toString() + "0");
+                txtview_resultview.setText(txtview_resultview.getText() + "0");
             }
         });
+
+        /*Internamente, un cálculo se realiza cada vez que se pulsa una tecla de operación*/
 
         btn_add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                txtview_resultview.setText(txtview_resultview.getText().toString() + "+");
+                if (txtview_resultview == null) {
+                    txtview_resultview.setText("");
+                } else {
+                    value1 = Float.parseFloat(txtview_resultview.getText() + "");
+                    addition = true;
+                    txtview_resultview.setText(null);
+                }
             }
         });
 
         btn_subtract.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                txtview_resultview.setText(txtview_resultview.getText().toString() + "-");
+                value1 = Float.parseFloat(txtview_resultview.getText() + "");
+                subtraction = true;
+                txtview_resultview.setText(null);
             }
         });
 
         btn_multiply.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                txtview_resultview.setText(txtview_resultview.getText().toString() + "*");
+                value1 = Float.parseFloat(txtview_resultview.getText() + "");
+                multiplication = true;
+                txtview_resultview.setText(null);
             }
         });
 
         btn_divide.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                txtview_resultview.setText(txtview_resultview.getText().toString() + "/");
+                value1 = Float.parseFloat(txtview_resultview.getText() + "");
+                division = true;
+                txtview_resultview.setText(null);
+            }
+        });
+
+        btn_result.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                value2 = Float.parseFloat(txtview_resultview.getText() + "");
+
+                if (addition == true) {
+                    txtview_resultview.setText(value1 + value2 + "");
+                    addition = false;
+                }
+
+                if (subtraction == true) {
+                    txtview_resultview.setText(value1 - value2 + "");
+                    subtraction = false;
+                }
+
+                if (multiplication == true) {
+                    txtview_resultview.setText(value1 * value2 + "");
+                    multiplication = false;
+                }
+
+                if (division == true) {
+                    txtview_resultview.setText(value1 / value2 + "");
+                    division = false;
+                }
+            }
+        });
+
+        btn_clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtview_resultview.setText("");
             }
         });
     }
